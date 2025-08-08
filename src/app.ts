06808a0634994346d9ae4ce1c9d1b2a91a976ef3
Soliman-soad <mdsolimansoad@gmail.com>
2025-08-08 18:34:55 +0600

    Add admin and certificate routes to the Express app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth";
 import testRoutes from "./routes/test";
 import { errorHandler } from "./middlewares/errorhandler";
+import adminRoutes from "./routes/admin";
+import certificateRoutes from "./routes/certificate";
 
 const app: Application = express();
 
@@ -12,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/certificate", certificateRoutes);
 
 app.use(errorHandler);
 
