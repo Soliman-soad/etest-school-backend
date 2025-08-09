1decfb901296f3240393d16e7c885c9c313ce1aa
Soliman-soad <mdsolimansoad@gmail.com>
2025-08-09 20:08:35 +0600

    error solve

--- src/eslint.config.mts
+++ src/eslint.config.mts
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import { defineConfig } from "eslint";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,10 @@ const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "refresh_secret";
 
 export const signAccessToken = (payload: object) => {
   const expiresIn = process.env.ACCESS_TOKEN_EXPIRES || "15m";
-  return jwt.sign(payload, ACCESS_SECRET, { expiresIn });
+  return jwt.sign(payload, ACCESS_SECRET, { expiresIn: expiresIn as jwt.SignOptions["expiresIn"] });
 };
 
 export const signRefreshToken = (payload: object) => {
   const expiresIn = process.env.REFRESH_TOKEN_EXPIRES || "7d";
-  return jwt.sign(payload, REFRESH_SECRET, { expiresIn });
+  return jwt.sign(payload, REFRESH_SECRET, { expiresIn: expiresIn as jwt.SignOptions["expiresIn"] });
 };
